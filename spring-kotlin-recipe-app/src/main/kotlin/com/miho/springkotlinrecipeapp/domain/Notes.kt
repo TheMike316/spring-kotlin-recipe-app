a981797b1983df71c05b882d10dff7ea94cea63f
mikeh <mike.hornegger@gmail.com>
2018-03-17 05:45:42 +0100

    Configure latest version of Kotlin

--- spring-kotlin-recipe-app/src/main/kotlin/com/miho/springkotlinrecipeapp/domain/Notes.kt
+++ spring-kotlin-recipe-app/src/main/kotlin/com/miho/springkotlinrecipeapp/domain/Notes.kt
@@ -1,17 +1,13 @@
 package com.miho.springkotlinrecipeapp.domain
 
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.OneToOne
-import javax.persistence.Entity
-import javax.persistence.Lob
+import javax.persistence.*
 
 @Entity
 class Notes(@OneToOne
-				 var recipe: Recipe? = null,
-				 
-				 @Lob
-				 var recipeNotes: String = "",
-				 
-				 @field: [Id  GeneratedValue(strategy = GenerationType.IDENTITY)]
-				 var id: Long = -1)
+            var recipe: Recipe? = null,
+
+            @Lob
+            var recipeNotes: String = "",
+
+            @field: [Id GeneratedValue(strategy = GenerationType.IDENTITY)]
+            var id: Long = -1)
